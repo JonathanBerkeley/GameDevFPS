be926705841565115594e57276d2e48d9f6e1ceb
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2020-11-27 02:53:57 +0000

    Main menu camera

---
+++ Assets/Scripts/MenuCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCamera : MonoBehaviour
+{
+    public GameObject focusTarget;
+    public float rotateSpeed = 5.0f;
+
+    void Update()
+    {
+        transform.RotateAround(focusTarget.transform.position, Vector3.up, rotateSpeed * Time.deltaTime);
+    }
+}
