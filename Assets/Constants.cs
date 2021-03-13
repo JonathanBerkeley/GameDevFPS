b6274d4760471a1ce08dd04c85c9aca96e9604c5
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2021-03-13 23:59:10 +0000

    Updated to unity 2020

--- Assets/Constants.cs
+++ Assets/Constants.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class Constants
+﻿public static class Constants
 {
-    public const string CLIENT_VERSION = "1.2.4";
+    public const string CLIENT_VERSION = "1.2.5";
 }
